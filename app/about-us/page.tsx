1dfa12c70acd024d063cd59b8c3c5ee0a887ab15
Taiwo H. Adelaja <adezeez@gmail.com>
2025-11-05 14:41:27 +0100

    sunpay changes

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,14 +1,13 @@
-// import Header from '@/components/Header'
-import SignIn from '../components/AboutUs/SignIn'
+// import SignIn from '../components/AboutUs/SignIn'
 
 
 
-export default function signIn() {
-  return (
-    <div className="min-h-screen bg-white">
+// export default function signIn() {
+//   return (
+//     <div className="min-h-screen bg-white">
         
-        <SignIn />
+//         <SignIn />
         
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
